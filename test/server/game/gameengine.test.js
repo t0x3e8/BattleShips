47a669c12617ff3ad8343b2b6a2b5810cbf4a2b9
t0x3e8 <t0x3e8@gmail.com>
2017-04-07 19:18:35 +0200

    Fire alarm I had to run

--- test/server/game/gameengine.test.js
+++ test/server/game/gameengine.test.js
@@ -92,7 +92,7 @@ describe('Game Engine requirements', sinon.test(function () {
         var player1 = new Player({ name: 'Player 1' });
         player1.setPawns([new Pawn({ type: 2, col: 0, row: 0 })]);
         var player2 = new Player({ name: 'Player 2' });
-        player2.setPawns([new Pawn({ type: 1, col: 0, row: 17 })]);
+        player2.setPawns([new Pawn({ type: 1, col: 0, row: 17 }), new Pawn({ type: 1, col: 1, row: 17 })]);
 
         var game = new Game();
         game.join(player1);
@@ -102,20 +102,33 @@ describe('Game Engine requirements', sinon.test(function () {
             expect(game.board.fields[0][0].pawn.type).to.be.equal(2);
             expect(game.board.fields[0][17].pawn.type).to.be.equal(1);
             expect(game.board.fields[0][16].pawn).to.be.null;
-            expect(range.length).to.be.equal(3);
+            expect(range.length).to.be.equal(2);
             expect(range[0].rowIndex).to.be.equal(16);
             expect(range[0].colIndex).to.be.equal(0);
             expect(range[1].rowIndex).to.be.equal(16);
             expect(range[1].colIndex).to.be.equal(1);
-            expect(range[2].rowIndex).to.be.equal(17);
-            expect(range[2].colIndex).to.be.equal(1);
             done();
         });
 
         game.start();
     }));
 
-    it.skip('Simulate the combat between the ships', sinon.test(function () {
+    it('Simulate the combat between the ships', sinon.test(function (done) {
+        var player1 = new Player({ name: 'Player 1' });
+        player1.setPawns([new Pawn({ type: 2, col: 0, row: 16 })]);
+        var player2 = new Player({ name: 'Player 2' });
+        player2.setPawns([new Pawn({ type: 1, col: 0, row: 17 }), new Pawn({ type: 1, col: 1, row: 17 })]);
 
+        var game = new Game();
+        game.join(player1);
+        game.join(player2);
+        game.on('gameWaiting', function () {
+            var range = game.board.getPawnRange(player2.pawns[0]);
+            expect(range.length).to.be.equal(2);
+
+            done();
+        });
+
+        game.start();
     }));
 }));
